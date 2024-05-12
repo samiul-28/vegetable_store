e65c16906f4bbfac3f2a3f4ffa0cb1b6d3a2cc31
tarekdinaji <mtarekhossain@gmail.com>
2024-05-12 17:41:33 +0600

    Product index added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,11 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-  
-   
+
+
   private
 
-    def 
-     permit(:name, :variety, :details, :price, :mfg_date, :expiry_date, :type)
+    def product_params
+      params.require(:product).permit(:name, :variety, :details, :price, :mfg_date, :expiry_date)
     end
 end
